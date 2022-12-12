81e6c77dd2e124dc3df1595231e2a220a378b021
Riley HP <r.hendricksonpike@gmail.com>
2022-12-12 12:21:56 -0400

    added window stylings. added welcome window. changed background. made windows resizable.

--- js/windows.js
+++ js/windows.js
@@ -1,29 +1,51 @@
-function resetWindows() {           
+function resetWindows() {
     $(".window-container").removeClass("selected-window")
+    $(".window-container").addClass("background-window")
 }
+
+$( function() {
+    $(".resizable").resizable({
+      handles: "n, e, s, w, ne, nw, se, sw"
+    });
+});
 
 $(".window-container").draggable({
     handle: ".window-header"
 });
 
-$("#btn-contact").click(function() {            
-    resetWindows()
-    $("#win-contact").show()
-    $("#win-contact").addClass("selected-window")
 
+$("#btn-contact").click(function() {
+    $("#win-contact").show()
+    resetWindows();
+    $("#win-contact").removeClass("background-window")
+    $("#win-contact").addClass("selected-window")
 })
 
-$("#btn-portfolio").click(function() {            
-    resetWindows()
+
+$("#btn-portfolio").click(function() {
     $("#win-portfolio").show()
+    resetWindows();
+    $("#win-portfolio").removeClass("background-window")
     $("#win-portfolio").addClass("selected-window")
 })
 
-$(".btn-win-close").click(function() {            
-    $(this).parent().hide()            
+function displayWelcome() {
+  $("#win-welcome").show()
+  resetWindows();
+  $("#win-welcome").removeClass("background-window")
+  $("#win-welcome").addClass("selected-window")
+};
+
+$(".btn-win-close").click(function() {
+    $(this).parent().hide()
 })
 
-$(".window-container").mousedown(function(){           
+$(".btn-confirm").click(function() {
+  $(this).parent().hide()
+})
+
+$(".window-container").mousedown(function(){
     resetWindows()
+    $(this).removeClass("background-window")
     $(this).addClass("selected-window")
 })
