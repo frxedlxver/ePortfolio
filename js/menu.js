ad1722eb360707cc13485c37822eb62788229a72
Riley HP <r.hendricksonpike@gmail.com>
2022-12-16 15:35:13 -0400

    added context menu

---
+++ js/menu.js
@@ -0,0 +1,53 @@
+
+
+$( "#desktop-menu" ).menu();
+
+// Hide context menu
+$(document).on('click',function(){
+    $(".context-menu").hide();
+    $("#txt_id").val("");
+});
+
+// disable right click and show custom context menu
+$(".desktop").on('contextmenu', function (e) {
+
+        var id = this.id;
+        $("#txt_id").val(id);
+
+        var top = e.pageY+5;
+        var left = e.pageX+5;
+
+        // Show contextmenu
+        $(".context-menu").toggle(100).css({
+            top: top + "px",
+            left: left + "px"
+        });
+
+        // disable default context menu
+        return false;
+});
+
+
+
+// disable context-menu from custom menu
+$('.context-menu').on('contextmenu',function(){
+    return false;
+});
+
+// Clicked context-menu item
+$('.context-menu li').on('click', function(){
+    var className = $(this).find("span:nth-child(1)").attr("class");
+    var titleid = $('#txt_id').val();
+    $( "#"+ titleid ).css( "background-color", className );
+    $(".context-menu").hide();
+});
+
+function setBackgroundBlack() {
+    $(".desktop").removeClass("bg1")
+    $(".desktop").addClass("bg0")
+}
+
+function setBackgroundBlue() {
+    $(".desktop").removeClass("bg0")
+    $(".desktop").addClass("bg1")
+}
